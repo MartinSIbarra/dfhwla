176fdca75528a4d079764cafe775fa5482d96c58
Martín Ibarra <martinibarra@gmail.com>
2025-06-28 11:49:55 -0300

    asd

--- images/ngrok-nginx-proxy/scripts/ngrok-start.sh
+++ images/ngrok-nginx-proxy/scripts/ngrok-start.sh
@@ -19,3 +19,5 @@ log "$root_path" "ngrok_tunnel_port: $ngrok_tunnel_port" "$0"
 log "$root_path" "ngrok_log_file: $log_file" "$0"
 [ "$root_path" == "$HOME" ] && ngrok http $ngrok_tunnel_port --url=$ngrok_tunnel_url --authtoken=$ngrok_auth_token --log=$log_file
 log "$root_path" "Ngrok tunnel started." "$0"
+
+chown "1000":"1000" "$log_file"
