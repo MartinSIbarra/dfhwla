b0aed885c22219a999fe27cc2e1629fb68b8fc22
Martín Ibarra <martinibarra@gmail.com>
2025-06-27 23:44:09 -0300

    df

---
+++ Makefile
@@ -0,0 +1,5 @@
+build-ngrok-nginx-proxy:
+	./common/helpers/build.sh "ngrok-nginx-proxy" "alpine"
+	
+build-ngrok-nginx-proxy:
+	./common/helpers/build.sh "ngrok-nginx-proxy" "alpine"
--- common/helpers/build.sh
+++ common/helpers/build.sh
@@ -7,11 +7,11 @@ image_tag=$2
 [ -z "$image_name" ] && { echo "Image name is required"; exit 1; }
 [ -z "$image_tag" ] && { echo "Image tag is required"; exit 1; }
 
-common_scripts=(./common/scripts/*)
+common_scripts=(./common/scripts/*) 
 image_path="./images/$image_name"
 image_scripts_path="$image_path/scripts"
 
-if [ ! -d "$image_scripts_path" ]; then
+if [ -d "$image_scripts_path" ]; then
     for script in "${common_scripts[@]}"; do
         cp "$script" "$image_scripts_path/"
     done
@@ -20,8 +20,8 @@ fi
 docker rmi -f "$image_name:$image_tag" 2>/dev/null || true
 docker build -t "$image_name:$image_tag" "$image_path"
 
-if [ ! -d "$image_scripts_path" ]; then
+if [ -d "$image_scripts_path" ]; then
     for script in "${common_scripts[@]}"; do
-        rm "$image_scripts_path/$script"
+        rm "$image_scripts_path/$(basename $script)"
     done
 fi
