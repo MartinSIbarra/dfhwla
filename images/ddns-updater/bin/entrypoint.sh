91138855952eb8ddd9b1548581297154df4bec65
Martín Ibarra <martinibarra@gmail.com>
2025-07-01 09:04:35 -0300

    asd

--- images/ddns-updater/bin/entrypoint.sh
+++ images/ddns-updater/bin/entrypoint.sh
@@ -14,15 +14,16 @@ last_duckdns_update_time=""
     $BIN_PATH/ddns-updt.sh
     duckdns_update_time=$(jq -r '.ddns.duckdns_update_time' "$PARAMS_FILE")
     [ -z "$duckdns_update_time" ] && duckdns_update_time=3600  # Default if not set
+
+    if [ -z "$last_duckdns_update_time" ]; then
+      log "Started background process to update DDNS(DuckDNS) every $duckdns_update_time seconds."
+      last_duckdns_update_time="$duckdns_update_time"
+    elif [ "$duckdns_update_time" != "$last_duckdns_update_time" ]; then
+      log "Update time to update DDNS(DuckDNS) changed from every $last_duckdns_update_time seconds to every $duckdns_update_time seconds."
+      last_duckdns_update_time="$duckdns_update_time"
+    fi
     sleep $duckdns_update_time
   done
-  if [ -z "$last_duckdns_update_time" ]; then
-    log "Started background process to update DDNS(DuckDNS) every $duckdns_update_time seconds."
-    last_duckdns_update_time="$duckdns_update_time"
-  elif [ "$duckdns_update_time" != "$last_duckdns_update_time" ]; then
-    log "Update time to update DDNS(DuckDNS) changed from every $last_duckdns_update_time seconds to every $duckdns_update_time seconds."
-    last_duckdns_update_time="$duckdns_update_time"
-  fi
 ) &
 
 wait
